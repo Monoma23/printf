86961b8b738de2a4e30aa2fd168c4878cbe081cd
Mustapha KHALID <mosstaf096@gmail.com>
2023-08-21 21:42:55 -0700

    done

---
+++ get_flags.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * get_flags - calculates active flags
+ * @format: fotmatted string in which to print the arguments
+ * @i: take a parameter
+ * Return: Flags
+ */
+int get_flags(const char *format, int *i)
+{
+	/* - + 0 # ' ' */
+	/* 1 2 4 8 16 */
+	int j, current_i;
+	int flags = 0;
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_CH[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+
+	for (current_i = *i + 1; format[current_i] != '\0'; current_i++)
+	{
+		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+			if (format[current_i] == FLAGS_CH[J])
+			{
+				flags |= FLAGS_ARR[j];
+				break;
+			}
+		if (FLAGS_CH[j] == 0)
+			break;
+	}
+	*i = current_i - 1;
+	return (flags);
+}
